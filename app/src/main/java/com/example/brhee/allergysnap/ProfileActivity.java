4126bfc680c4f480bd1a320f0988fe62fbdfa638
Tomasz Parzadka <tomaszparzadka@pal-nat186-141-91.itap.purdue.edu>
2018-09-22 17:29:27 -0400

    Created ProfileActivity and layout.

---
+++ app/src/main/java/com/example/brhee/allergysnap/ProfileActivity.java
@@ -0,0 +1,26 @@
+package com.example.brhee.allergysnap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView name, username;
+
+    private CircleImageView userProfileImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        name = (TextView) findViewById(R.id.my_profile_full_name);
+        username = (TextView) findViewById(R.id.my_profile_username);
+        userProfileImage = (CircleImageView) findViewById(R.id.my_profile_picture);
+
+    }
+}
