009193d5fa9a84ca763a7f002fb6ee0a0ea4a45c
Ashwin Prasad <APrasad021@gmail.com>
2018-10-01 21:55:06 -0400

    Initial structure to Edit Profile page and class

--- app/src/main/java/com/example/brhee/allergysnap/ProfileDetailActivity.java
+++ app/src/main/java/com/example/brhee/allergysnap/ProfileDetailActivity.java
@@ -2,12 +2,44 @@ package com.example.brhee.allergysnap;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
-public class ProfileDetailActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileDetailActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText editFirstName, editLastName, editEmail, editPassword, editPasswordConfirm, editDOB;
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_detail);
+
+        editFirstName = findViewById(R.id.first_name_text);
+        editLastName = findViewById(R.id.last_name_text);
+        editEmail = findViewById(R.id.email_text);
+        editPassword = findViewById(R.id.password_text);
+        editPasswordConfirm = findViewById(R.id.password_conf_text);
+        editDOB = findViewById(R.id.dob_text);
+
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = findViewById(R.id.progressBar);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.submit_button:
+
+                break;
+            case R.id.deactivate_button:
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/brhee/allergysnap/User.java
+++ app/src/main/java/com/example/brhee/allergysnap/User.java
@@ -2,6 +2,8 @@ package com.example.brhee.allergysnap;
 
 public class User {
     protected String username, email, DOB, fName, lName;
+    protected boolean validName = false;
+    protected boolean hasPFP = false;
 
     public User() {
     }
@@ -9,6 +11,13 @@ public class User {
     public User(String username, String email) {
         this.username = username;
         this.email = email;
+    }
 
+    public User(String username, String email, String DOB, String fName, String lName) {
+        this.username = username;
+        this.email = email;
+        this.DOB = DOB;
+        this.fName = fName;
+        this.lName = lName;
     }
 }
