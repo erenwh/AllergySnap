a3c80e91e823ea01219de42687855db9e394ae30
Tomasz Parzadka <tparzadk@purdue.edu>
2018-10-05 00:31:09 -0400

    Changed logout and DB retrieval.

--- app/src/main/java/com/example/brhee/allergysnap/ProfileActivity.java
+++ app/src/main/java/com/example/brhee/allergysnap/ProfileActivity.java
@@ -1,18 +1,21 @@
 package com.example.brhee.allergysnap;
 
-import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
@@ -20,18 +23,14 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    Context context;
-
-    private FirebaseAuth firebaseAuth;
-
-    private DatabaseReference profileUser;
-
     private Button signout_button;
-
     private TextView fullname, username;
-
-    private String currentUserID;
-
+    private FirebaseDatabase mFirebaseDatabase;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseAuth mAuth;
+    private DatabaseReference myRef;
+    private String userID;
+    private User userObj;
     private CircleImageView userProfileImage;
 
     @Override
@@ -39,6 +38,51 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference("Users");
+        final FirebaseUser user = mAuth.getCurrentUser();
+
+        fullname = (TextView) findViewById(R.id.profile_full_name);
+        username = (TextView) findViewById(R.id.profile_username);
+
+        if (user != null) {
+            userID = user.getUid();
+        }
+        if (userID == null) {
+            startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+        }
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Query userData = myRef;
+                    userData.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            if (dataSnapshot.exists()) {
+                                userObj = dataSnapshot.child(userID).getValue(User.class);
+                                setValues();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                } else {
+                    // User is signed out
+                    startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                }
+            }
+        };
+
+
+        /*
         firebaseAuth = FirebaseAuth.getInstance();
         currentUserID = firebaseAuth.getCurrentUser().getUid();
 
@@ -50,18 +94,25 @@ public class ProfileActivity extends AppCompatActivity {
         // This will cause the program to crash if a user is not found in the database.
         //userProfileImage = (CircleImageView) findViewById(R.id.profile_picture);
 
+
+
         profileUser.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 if (dataSnapshot.exists()) {
-                    //String myFirstName = dataSnapshot.child("firstname").getValue().toString();
-                    //String myLastName = dataSnapshot.child("lastname").getValue().toString();
+                    String myFirstName = dataSnapshot.child("fName").getValue().toString();
+                    String myLastName = dataSnapshot.child("lName").getValue().toString();
                     String myUsername = dataSnapshot.child("username").getValue().toString();
                     //String myProfileImage = dataSnapshot.child("profilepicture").getValue().toString();
 
                     //Picasso.get().load(myProfileImage).placeholder(R.drawable.ic_profile_default).into(userProfileImage);
 
-                    //fullname.setText(myFirstName + " " + myLastName);
+                    if (myFirstName.equals("") && myLastName.equals("")) {
+                        fullname.setText("Profile Name");
+                    }
+                    else {
+                        fullname.setText(myFirstName + " " + myLastName);
+                    }
                     username.setText(myUsername);
                 }
             }
@@ -70,23 +121,54 @@ public class ProfileActivity extends AppCompatActivity {
             public void onCancelled(DatabaseError databaseError) {
 
             }
-        });
+        });*/
+
 
         signout_button = (Button) findViewById(R.id.profile_signout_button);
 
         signout_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                firebaseAuth.signOut();
+                mAuth.signOut();
                 finish();
                 startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
-
             }
         });
 
-    }
 
-    private void InitializeFields() {
 
     }
+    private void setValues() {
+        if (userObj.username != null) {
+            username.setText(userObj.username);
+        }
+
+        if(userObj.fName != null || userObj.lName != null ) {
+            if (userObj.fName == null) {
+                fullname.setText(" " + userObj.lName);
+            }
+            else if (userObj.lName == null) {
+                fullname.setText(userObj.fName+ " ");
+            }
+            else {
+                fullname.setText(userObj.fName + " " + userObj.lName);
+            }
+        }
+        else {
+            fullname.setText("Profile Name");
+        }
+    }
+
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
 }
