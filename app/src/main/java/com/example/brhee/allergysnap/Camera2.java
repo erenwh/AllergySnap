f7c027afbec2a3df127a7ee1e5d58d2d29c52ef2
rhbrian <brianhrhee@gmail.com>
2018-11-01 20:31:29 -0400

    added picture functionality

--- app/src/main/java/com/example/brhee/allergysnap/Camera2.java
+++ app/src/main/java/com/example/brhee/allergysnap/Camera2.java
@@ -2,13 +2,19 @@ package com.example.brhee.allergysnap;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import android.Manifest;
@@ -16,18 +22,26 @@ import android.Manifest;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.Frame;
 import com.google.android.gms.vision.MultiDetector;
 import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Camera2 extends AppCompatActivity {
 
     private SurfaceView sv;
     private TextView tv;
+    private BarcodeDetector barcodeDetector;
+    private TextRecognizer textRecognizer;
+    private MultiDetector multiDetector;
+    private CameraSource cameraSource;
 
     private static final int requestPermissionID = 101;
 
@@ -37,83 +51,29 @@ public class Camera2 extends AppCompatActivity {
         setContentView(R.layout.camera2);
         sv = (SurfaceView)findViewById(R.id.sv_barcode);
         tv = (TextView) findViewById(R.id.tv_barcode);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+
         startCameraSource();
     }
 
-    // Old CameraSource for Barcode only
-    /*
-    private void createCameraSource() {
-
-        BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
-
-        final CameraSource cs = new CameraSource.Builder(this, barcodeDetector).setAutoFocusEnabled(true).setRequestedPreviewSize(1600, 1024).build();
-
-        sv.getHolder().addCallback(new SurfaceHolder.Callback() {
-            @Override
-            public void surfaceCreated(SurfaceHolder surfaceHolder) {
-                if (ActivityCompat.checkSelfPermission(Camera2.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                try {
-                    cs.start(sv.getHolder());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                cs.stop();
-            }
-        });
-
-
-        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
-            @Override
-            public void release() {
-
-            }
-
-            @Override
-            public void receiveDetections(Detector.Detections<Barcode> detections) {
-                final SparseArray<Barcode> barcodes = detections.getDetectedItems();
-
-                if (barcodes.size() > 0) {
-                    Intent intent = new Intent();
-                    intent.putExtra("barcode", barcodes.valueAt(0)); // Gets the latest barcode from the array
-                    setResult(CommonStatusCodes.SUCCESS, intent);
-                    finish();
-                }
-            }
-        });
-    }*/
+    public void TakePicture(View v) {
+        cameraSource.stop();
+    }
 
     private void startCameraSource() {
 
 
         // Create the BarcodeDetector
-        final BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+        barcodeDetector = new BarcodeDetector.Builder(this).build();
 
         // Create the TextRecognizer
-        final TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+        textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
         // Create MultiDetector
-        MultiDetector multiDetector = new MultiDetector.Builder().add(barcodeDetector).add(textRecognizer).build();
+        multiDetector = new MultiDetector.Builder().add(barcodeDetector).add(textRecognizer).build();
 
         // Create CameraSource
-        final CameraSource cameraSource = new CameraSource.Builder(this, multiDetector).setAutoFocusEnabled(true).setRequestedPreviewSize(1600, 1024).build();;
+        cameraSource = new CameraSource.Builder(this, multiDetector).setAutoFocusEnabled(true).setRequestedPreviewSize(1600, 1024).build();;
 
         if (!multiDetector.isOperational()) {
             Log.d("cam2activity","Detector dependencies not loaded yet");
@@ -180,7 +140,7 @@ public class Camera2 extends AppCompatActivity {
                                 for(int i=0;i<items.size();i++){
                                     TextBlock item = items.valueAt(i);
                                     stringBuilder.append(item.getValue());
-                                    stringBuilder.append("\n");
+                                    stringBuilder.append("-");
                                 }
                                 tv.setText(stringBuilder.toString());
                             }
