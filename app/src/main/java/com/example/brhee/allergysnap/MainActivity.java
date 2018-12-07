4902d47fb7192c1a59b892f9a3a8f799ca0a14ef
Ashwin Prasad <APrasad021@gmail.com>
2018-12-06 23:58:19 -0500

    Add import

--- app/src/main/java/com/example/brhee/allergysnap/MainActivity.java
+++ app/src/main/java/com/example/brhee/allergysnap/MainActivity.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Build;
